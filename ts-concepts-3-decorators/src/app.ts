51e2da341619fac5efe974e1f4504a8323027897
Pranav <naringrekarpranav6@live.com>
2023-01-08 00:06:51 +0530

    ts-concepts-decorators

--- ts-concepts-3-decorators/src/app.ts
+++ ts-concepts-3-decorators/src/app.ts
@@ -11,11 +11,7 @@ function Log2(target: any, key: string, descriptor: PropertyDescriptor) {
 }
 
 //_ ------------- METHOD DECORATOR -----------
-function Log3(
-  target: any,
-  key: string,
-  descriptor: PropertyDescriptor
-): PropertyDescriptor {
+function Log3(target: any, key: string, descriptor: PropertyDescriptor) {
   console.log('Function Decorator');
   console.log(target, key, descriptor);
 }
@@ -96,8 +92,8 @@ console.log(person);
 
 //_ ------------- METHOD DECORATOR WITH RETURN VALUE -----------
 function Autobind(
-  target: any,
-  key: string,
+  _target: any,
+  _methodName: string,
   descriptor: PropertyDescriptor
 ): PropertyDescriptor {
   const originalMethod = descriptor.value;
@@ -112,10 +108,10 @@ function Autobind(
   return adjustedDescriptor;
 }
 
-@Autobind
 class Printer {
   message = 'This works';
 
+  @Autobind
   showMessage() {
     console.log(this.message);
   }
@@ -123,4 +119,84 @@ class Printer {
 const p = new Printer();
 const button = document.querySelector('button')!;
 // button.addEventListener('click', p.showMessage.bind(p));
-button.addEventListener('click', p.showMessage.bind(p));
+button.addEventListener('click', p.showMessage);
+
+// _ VALIDATION DECORATORS
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      'required',
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      'positive',
+    ],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) return true;
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+
+  @PositiveNumber
+  price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const titleElement = document.getElementById('title') as HTMLInputElement;
+  const priceElement = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleElement.value;
+  const price = +priceElement.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    throw new Error('Invalid Input, please try again!');
+  }
+  console.log(createdCourse);
+});
